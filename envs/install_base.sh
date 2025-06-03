c8dda50de9d709ad08345eb054fea828f3744d5d
Qi Wang <1254364001@qq.com>
2025-06-03 23:27:15 +0800

    update outdated and invalid packages

---
+++ envs/install_base.sh
@@ -0,0 +1,6 @@
+# 注意unset http_proxy
+cd ~
+wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.4.post1/flash_attn-2.7.4.post1+cu12torch2.6cxx11abiTRUE-cp311-cp311-linux_x86_64.whl
+bash Miniconda3-latest-Linux-x86_64.sh
+source ~/.bashrc
